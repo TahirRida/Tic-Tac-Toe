0097f8839cc71872aa92261b741843cf21019e48
TahirRida <tahirrida52@gmail.com>
2023-04-21 15:14:23 +0000

    I changed the strategy by which i will code the game, the game might be played on console right now, the UI will be available soon

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Making the grid of the game
 (function createGameBoard() {
-  let gameBoard = [[], [], []];
   let gameGrid = document.querySelector(".game");
   let id = 1; // This id will be used as an attribute to the grid Element that will be added
   for (let i = 0; i < 3; i++) {
@@ -17,15 +16,11 @@
 
 //Creating the game board
 
-let gameBoard = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
 //Getting the user's choice
-let userChoice = "";
-function getUserChoice() {
+let userChoice = "x";
+/*function getUserChoice() {
   window.addEventListener("click", (e) => {
     if (
       userChoice === "" &&
@@ -37,36 +32,79 @@ function getUserChoice() {
     console.log(userChoice);
   });
 }
+*/
 //Constructor of players
-function player(userChoice) {
-  this.choice = userChoice;
+function player(userChoice, isComputer) {
+  let player = {};
+  player.choice = userChoice;
+  player.isComputer = isComputer;
+  return player;
 }
 //Checking wether the game has started or not in order to give the player the possibility to change his choice
 
 function gameHasStarted() {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (gameBoard[i][j] != "") {
-        return true;
-      }
+  for (let i = 0; i < 9; i++) {
+    if (gameBoard[i] != "") {
+      return true;
     }
   }
   return false;
 }
+
+//displaying gameBoard
+function display(gameBoard) {
+  for (let i = 0; i < gameBoard.length; i += 3) {
+    console.log(`${gameBoard[i]}|${gameBoard[i + 1]}|${gameBoard[i + 2]}`);
+    console.log("");
+  }
+  console.log("-------------------------");
+}
 //Making user round
-function userRound(userChoice) {
-  //get user round and update gameBoard
+function userRound(userChoice, isComputerRound) {
+  let cell; //This is the cell that will be filled due to this Round
+  if (!isComputerRound) {
+    cell = prompt("choose the number of the cell you wanna insert x into ");
+    while (gameBoard[cell] != " ") {
+      cell = prompt("choose an empty cell");
+    }
+  } else {
+    let i = Math.floor(Math.random() * 9);
+    while (gameBoard[i] != " ") {
+      i = Math.floor(Math.random() * 9);
+    }
+    cell = i;
+  }
+  gameBoard[cell] = userChoice;
 }
 //Checking wether the game has ended
 function weHaveAWinner() {
   if (gameHasStarted()) {
-    //check if winning conditions are verified
   }
+  return false; // This statement was written only to see the gameon the console, remove it after
+}
+//checking wether the gameBoard is full
+function isTaken(element, index, array) {
+  return element != " ";
+}
+function isFull(gameBoard) {
+  return gameBoard.every(isTaken);
+}
+function gameEnded() {
+  if (weHaveAWinner() || isFull(gameBoard)) {
+    return true;
+  }
+  return false;
 }
 // The game Function
 
 function game() {
-  while (!weHaveAWinner()) {
-    //userRound(userChoice);
+  let human = player("X", false);
+  let computer = player("O", true);
+  while (!gameEnded()) {
+    display(gameBoard);
+    userRound(human.choice, human.isComputer);
+    userRound(computer.choice, computer.isComputer);
+    display(gameBoard);
   }
 }
+game();
